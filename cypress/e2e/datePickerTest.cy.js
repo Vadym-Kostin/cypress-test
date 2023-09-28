c54cbe663b345275186c2caf7f0cee6eed5b8432
Vadym-Kostin <felixfelices007@gmail.com>
2023-09-28 21:27:14 +0300

    removed unnecessary part of code from Datepicker test

--- cypress/e2e/datePickerTest.cy.js
+++ cypress/e2e/datePickerTest.cy.js
@@ -11,13 +11,8 @@ describe("Datepicker page suite", () => {
         menuComponent.openTabsByValues(tab, subTab);
         const datepicker = new DatepickerPage();
         datepicker.getCommonDatepicker().click();
-        const todayDate = moment().format("MMM Do Y");
-        const parts = todayDate.split(' ');
-        const todayDateWithoutSuffix = parts[1].replace(/\D/g, "");
-        const month = parts[0];
-        const year = parts[2];
-        const newDateString = `${month} ${todayDateWithoutSuffix}, ${year}`;
+        const todayDate = moment().format("MMM D, Y");
         datepicker.getTodayDateInCalendar().click();
-        datepicker.getCommonDatepicker().should("have.value", newDateString);
+        datepicker.getCommonDatepicker().should("have.value", todayDate);
     });
 });
